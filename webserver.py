11bd3165ffc435231f747526087ef01f21a9c6cb
Versi-Beta <drunfty@gmail.com>
2026-01-28 16:06:14 -0500

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -9,11 +9,12 @@ def home():
     return "Nori is good!"
 
 def run():
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     t = Thread(target=run)
 
     t.start()
+
 
 
